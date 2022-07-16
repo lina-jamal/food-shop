ecc05ad4e06987b47d40a7bdda3d8e8015efb2a5
lina-jamal <linahjamal89@gmail.com>
2022-07-17 00:43:05 +0300

    finish the fist page

--- food_shop/lib/screen/home_page.dart
+++ food_shop/lib/screen/home_page.dart
@@ -6,10 +6,6 @@ class PageOne extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: const Text('Page One'),
-      //   // leading: BackButton(color: Colors.black),
-      // ),
       body: Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
@@ -32,18 +28,77 @@ class PageOne extends StatelessWidget {
               ),
               Container(
                   height: MediaQuery.of(context).size.height * 0.5,
-
-                  // width: 350,
+                  width: MediaQuery.of(context).size.width,
                   decoration: BoxDecoration(
                       color: Colors.white,
                       border: Border.all(
                         color: Colors.white,
                       ),
-                      borderRadius: BorderRadius.only(
+                      borderRadius: const BorderRadius.only(
                         topLeft: Radius.circular(100),
                         topRight: Radius.circular(100),
                       )),
-                  child: null),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const SizedBox(
+                        height: 70,
+                      ),
+                      const Text(
+                        'The Fasted Food In ',
+                        style: TextStyle(
+                            letterSpacing: 1.5,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 32),
+                      ),
+                      const Text(
+                        'Delivery Food',
+                        style: TextStyle(
+                            letterSpacing: 1.5,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 32),
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      const Text(
+                        ' Our job is to falling your tummy with',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      const Text(
+                        'delicious food and fast delivery.',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Icon(
+                            Icons.rectangle,
+                            size: 12,
+                            color: Color.fromARGB(255, 231, 200, 99),
+                          ),
+                          Icon(
+                            Icons.more_horiz,
+                            size: 45,
+                            color: Color.fromARGB(225, 240, 234, 222),
+                          ),
+                        ],
+                      ),
+                      ElevatedButton(
+                        onPressed: () => print('object'),
+                        child: const Text('Get Started'),
+                        style: ElevatedButton.styleFrom(
+                          primary: const Color.fromRGBO(191, 53, 78, 1),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(32.0),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )),
             ],
           ),
         ),
